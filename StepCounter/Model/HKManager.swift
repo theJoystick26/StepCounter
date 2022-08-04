b19096a16d6bd443ac1bbd57add80374b2113f6f
theJoystick26 <adinjj@gmail.com>
2022-08-04 14:35:15 -0400

    Added calorie field
    Keeps track of calories and writes energy burned to health kit when stop is pressed

---
+++ StepCounter/Model/HKManager.swift
@@ -0,0 +1,44 @@
+//
+//  HKManager.swift
+//  StepCounter
+//
+//  Created by Adin Joyce on 8/3/22.
+//
+
+import Foundation
+import HealthKit
+
+class HKManager {
+    var healthStore: HKHealthStore?
+    var query: HKStatisticsCollectionQuery?
+    
+    func authorizeHealthKit(completion: @escaping (Bool) -> Void) {
+        if HKHealthStore.isHealthDataAvailable() {
+            healthStore = HKHealthStore()
+            
+            let calorieType = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!
+            
+            healthStore?.requestAuthorization(toShare: [calorieType], read: [], completion: { success, error in
+                if error != nil {
+                    print("Error \(error!)")
+                } else {
+                    completion(success)
+                }
+            })
+        }
+    }
+    
+    func writeToHealthKit(_ calories: Float, _ startTime: Date, _ endTime: Date) {
+        let calorieType = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!
+        let quantity = HKQuantity(unit: HKUnit.kilocalorie(), doubleValue: Double(calories))
+        let sample = HKQuantitySample(type: calorieType, quantity: quantity, start: startTime, end: endTime)
+        
+        healthStore?.save(sample, withCompletion: { success, error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            print("Success: \(success)")
+        })
+    }
+}
